fd53e14ac5c8118c918ca8d2ee719be5c7b4f017
Nemolite <g25092011@mail.ru>
2022-04-22 08:14:46 +0300

    class

---
+++ oop1.py
@@ -0,0 +1,16 @@
+class Plan:
+    color = "red"
+    parm = 12
+
+    def myfunc(self,a):
+        self.a = a
+    def myinner():
+        print('I am myinner')
+obj = Plan()
+obj.myfunc(2)
+print(obj.a)
+Plan.myinner()
+#obj.myinner()
+
+###################
+        
