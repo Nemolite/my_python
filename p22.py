f6971a4b4b4b87010bca51fcbca3961b3404ef70
Nemolite <g25092011@mail.ru>
2022-05-04 11:12:53 +0300

    fio

---
+++ p22.py
@@ -0,0 +1,32 @@
+from string import ascii_letters
+class Semelein:
+    _S_RUS = 'абвгдеёжзийклмнопрстуфхцчшщъыьэюя'
+    _S_RUS_UPPER = _S_RUS.upper()
+
+    def __init__(self,fio,old,ps,weight):
+        self.verification_fio(fio)
+
+        self.__fio = fio.split() # список
+        self.__old = old # целое число
+        self.__pass = ps # строка
+        self.__weight = weight
+
+    @classmethod
+    def verification_fio(cls,fio):
+        if type(fio) != str:
+            raise TypeError('ФИО должно быть строкой')
+
+        f= fio.split()
+        if len(f)!=3:
+            raise TypeError('Не верный формат ФИО')
+
+        letters = ascii_letters + cls._S_RUS + cls._S_RUS_UPPER
+        for s in f:
+            if len(s)<1:
+                raise TypeError('В ФИО должен быть хотя бы один символ')
+            if len(s.strip(letters))!=0:
+                raise TypeError('В ФИО нужно использовать только буквы')
+
+obj = Semelein('Вушняков Сергей Валерьевич',51,'123',83.200)
+print(obj.__dict__)
+
