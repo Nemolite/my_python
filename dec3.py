3e18f583c192f67ba4f6b09faff10fdb306813f6
Nemolite <g25092011@mail.ru>
2022-05-05 20:01:15 +0300

    coord

--- dec3.py
+++ dec3.py
@@ -1,4 +1,10 @@
 class Integer:
+
+    @classmethod
+    def verify_coord(cls,coord):
+        if type(coord) != int:
+            raise TypeError('Координата должна быть целым числом')
+
     def __set_name__(self, owner, name):
         self.name = '_' + name
 
@@ -6,7 +12,8 @@ class Integer:
         return instance.__dict__[self.name]
 
     def __set__(self, instance, value):
-       # print(f"__set__:{self.name} = {value}")
+        #print(f"__set__:{self.name} = {value}")
+        self.verify_coord(value)
         instance.__dict__[self.name] = value
 
 class Point3D:
@@ -18,13 +25,6 @@ class Point3D:
         self.x = x
         self.y = y
         self.z = z
-
-    @classmethod
-    def verify_coord(cls,coord):
-        if coord != int:
-            raise TypeError('Координата должна быть целым числом')
-
-
 
 coord = Point3D(2,3,4)
 print(coord.__dict__)
