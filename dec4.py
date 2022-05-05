9a95626f5bcc85ade355d9710d6caa790832ada1
Nemolite <g25092011@mail.ru>
2022-05-05 20:12:10 +0300

    non date

---
+++ dec4.py
@@ -0,0 +1,32 @@
+class Integer:
+
+    @classmethod
+    def verify_coord(cls,coord):
+        if type(coord) != int:
+            raise TypeError('Координата должна быть целым числом')
+
+    def __set_name__(self, owner, name):
+        self.name = '_' + name
+
+    def __get__(self, instance, owner):
+        #return instance.__dict__[self.name]
+        return getattr(instance,self.name)
+
+    def __set__(self, instance, value):
+        #print(f"__set__:{self.name} = {value}")
+        self.verify_coord(value)
+        #instance.__dict__[self.name] = value
+        setattr(instance,self.name,value)
+
+class Point3D:
+    x = Integer()
+    y = Integer()
+    z = Integer()
+
+    def __init__(self,x,y,z):
+        self.x = x
+        self.y = y
+        self.z = z
+
+coord = Point3D(2,3,4)
+print(coord.__dict__)
